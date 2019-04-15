892016542261b46055ead981804cfb54641dd5f6
MoistSenpai <supercrab115@gmail.com>
2019-04-15 12:00:27 -0500

    Fixed typo, moved things to config

--- Routs/sharex.js
+++ Routs/sharex.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
-  
+
 module.exports.run = (req, res, client) => {
   let key = req.body.key;
   let name = req.body.name;
 
   // Check secret
-  if (key !== config.sharex_key)
+  if (key !== client.config.sharex.key)
     return res.end(`Unauthorized.`);
   // Upload file
-  fs.writeFileSync(`/var/www/pics/images/${name}.png`, req.file.buffer);
-  return res.end(`https://pics.quarkz.xyz/images/${name}.png`);
+  fs.writeFileSync(`${client.config.sharex.path}/${name}.png`, req.file.buffer);
+  return res.end(`${client.config.sharex.url}/images/${name}.png`);
 };
 
 module.exports.type = 'POSTY';
