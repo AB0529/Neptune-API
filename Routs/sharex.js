6cbd6679ebc33a58c9ddecb283f279f03514ec64
MoistSenpai <supercrab115@gmail.com>
2019-04-15 21:10:46 -0500

    Added gif support

--- Routs/sharex.js
+++ Routs/sharex.js
@@ -1,15 +1,20 @@
 const fs = require('fs');
+const isGif = require('is-gif');
 
 module.exports.run = (req, res, client) => {
-  let key = req.body.key;
-  let name = req.body.name;
+	let key = req.body.key;
+	let name = req.body.name;
 
-  // Check secret
-  if (key !== client.config.sharex.key)
-    return res.end(`Unauthorized.`);
-  // Upload file
-  fs.writeFileSync(`${client.config.sharex.path}/${name}.png`, req.file.buffer);
-  return res.end(`${client.config.sharex.url}/images/${name}.png`);
+	// Check secret
+	if (key !== client.config.sharex.key)
+		return res.end(`Unauthorized.`);
+	// Upload file
+	if (isGif(req.file.buffer)) {
+		fs.writeFileSync(`${client.config.sharex.path}/${name}.gif`, req.file.buffer);
+		return res.end(`${client.config.sharex.url}/${name}.gif`);
+	}
+	fs.writeFileSync(`${client.config.sharex.path}/${name}.png`, req.file.buffer);
+	return res.end(`${client.config.sharex.url}/${name}.png`);
 };
 
 module.exports.type = 'POSTY';
